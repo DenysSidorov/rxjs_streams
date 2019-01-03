35fa021d1847d13a4974ee7bd8cb7eb7f8e9bef1
Denys Sidorov <000scorpions000@gmail.com>
2019-01-03 17:26:53 +0200

    add data, error, complete handlers

--- src/rxFolder/index.js
+++ src/rxFolder/index.js
@@ -1,24 +1,43 @@
 import Rx from 'rxjs';
 
-const stream$ = Rx.Observable.create((observer)=>{
+const stream$ = Rx.Observable.create((observer) => {
     observer.next('One');
 
-    setTimeout(()=>{
+    setTimeout(() => {
         observer.next('New data after 3 sec');
-    },3000)
+        /** Throw error */
+        // observer.error('Baaaad');
+    }, 3000)
 
 
-    setTimeout(()=>{
+    setTimeout(() => {
         observer.next('New data after 5 sec');
-    },5000)
+    }, 5000)
 
 
     observer.next('Two');
+
+    /** Complete stream */
+    // setTimeout(() => {
+    //     observer.complete();
+    // }, 3500)
+
 });
 
 
-stream$.subscribe((data)=>{
-    console.log(data);
-})
+stream$.subscribe(
+    /** onData */
+    (data) => {
+        console.log(data);
+    },
+    /** error handler */
+    (error) => {
+        console.log('Erorr:( - ', error);
+    },
+    /** complete handler */
+    () => {
+        console.log('Observer ended his work!');
+    }
+)
 
 console.log(Rx);
